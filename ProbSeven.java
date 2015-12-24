539de36b6f37abbe18f59bd93a9c9a560febea3e
Wayne Zhang <renirest@gmail.com>
2015-12-24 02:58:55 -0500

    10001st prime
    By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can
    see that the 6th prime is 13.
    
    What is the 10 001st prime number?

---
+++ ProbSeven.java
@@ -0,0 +1,35 @@
+public class ProbSeven{
+	public static void main(String args[]){
+		System.out.println(findPrime(10001));
+	
+	}
+	public static int findPrime(int pos){
+		int count = 0;
+		int counter = 1;
+		while(count < pos){
+			counter++;
+			if(isPrime(counter))
+				count++;
+		}
+		return counter;
+	}
+	public static boolean isPrime(int num){
+		if(num==2)
+			return true;
+		else if(num%2==0)
+			return false;
+		else{
+			for(int i = 3; i<(num);i++){
+				if(num%i==0)
+					return false;
+			
+			}
+			return true;
+		
+		
+		}
+	
+	
+	}
+
+}
