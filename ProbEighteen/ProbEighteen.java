ed705ccee4705141d515eb06ca0fc5fa8649a287
Wayne Zhang <renirest@gmail.com>
2015-12-25 12:27:26 -0500

    Maximum path sum I
    By starting at the top of the triangle below and moving to adjacent
    numbers on the row below, the maximum total from top to bottom is 23.
    
    3
    7 4
    2 4 6
    8 5 9 3
    
    That is, 3 + 7 + 4 + 9 = 23.
    
    Find the maximum total from top to bottom of the triangle below:
    
    75
    95 64
    17 47 82
    18 35 87 10
    20 04 82 47 65
    19 01 23 75 03 34
    88 02 77 73 07 63 67
    99 65 04 28 06 16 70 92
    41 41 26 56 83 40 80 70 33
    41 48 72 33 47 32 37 16 94 29
    53 71 44 65 25 43 91 52 97 51 14
    70 11 33 28 77 73 17 78 39 68 17 57
    91 71 52 38 17 14 91 43 58 50 27 29 48
    63 66 04 68 89 53 67 30 73 16 69 87 40 31
    04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
    
    NOTE: As there are only 16384 routes, it is possible to solve this
    problem by trying every route. However, Problem 67, is the same
    challenge with a triangle containing one-hundred rows; it cannot be
    solved by brute force, and requires a clever method! ;o)

---
+++ ProbEighteen/ProbEighteen.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+public class ProbEighteen{
+	public static void main(String []args){
+		String fileName = "Triangle.txt";
+		String text = "";
+		String line = "";
+		int counter = 0;
+		try{
+			FileReader fr = new FileReader(fileName);
+			BufferedReader br = new BufferedReader(fr);
+			
+			while((line = br.readLine())!=null){
+				text+=line+" ";
+				counter++;
+			}
+			br.close();
+		}
+		catch(Exception e){
+			System.out.println("Error");
+		}
+		int [][] triangle = new int [counter][counter];
+		int theCount = 0;
+		for(int i = 0; i<counter; i++){
+			for(int j = 0; j<counter; j++){
+				triangle[i][j] = 0;
+			}
+		}
+		for(int i = 0; i< counter; i++){
+			for(int j = 0; j<i+1;j++){
+				String nl = text.substring(theCount,theCount+2);
+				
+				triangle[i][j] = Integer.parseInt(nl);
+				theCount +=3;
+			}
+		}
+		for(int i = counter-2; i>=0; i--){
+			for(int j = 0; j<i+1; j++){
+				
+				triangle[i][j] = triangle[i][j] + Math.max(triangle[i+1][j],triangle[i+1][j+1]);
+				
+			}
+		}
+		System.out.println(triangle[0][0]);
+	}
+
+
+}
